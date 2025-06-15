a4bb9300edd8b95698e2fd30f7f9eda1cf00d403
mike-nut <s243325@stn.nagaokaut.ac.jp>
2025-06-15 20:38:41 +0900

    [feat] フロントエンドの表示とAPI関連の問題の修正

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://backend:8080/api/:path*',
+      },
+    ];
+  },
+
+
 };
 
 export default nextConfig;
